2e0d7bb72d08f5f19410954ac24b6bf86ff580ad
hunsuk1546 <hunsuk1546@naver.com>
2018-06-08 16:44:48 +0900

    Add files via upload

--- app/src/androidTest/java/com/example/user/eosandroid/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/user/eosandroid/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.user.eosandroid;
+package io.plactal.eoscommander;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -10,17 +10,17 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.user.eosandroid", appContext.getPackageName());
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        assertEquals("io.plactal.eoscommanderfordevelopers", appContext.getPackageName());
     }
 }
